ea63429a133fae9b121852148a6c8e03e118e02b
shilivvi <shilivvi98@gmail.com>
2022-10-13 16:11:29 +0300

    Added Auth controller

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Password;
+use function bcrypt;
+use function response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => [
+                'required',
+                'confirmed',
+                Password::min(8)->mixedCase()->numbers()->symbols()
+            ],
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    public function login(Request $request){
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exist:users,email',
+            'password' => [
+                'required',
+            ],
+        ]);
+
+        if(!Auth::attempt($credentials)){
+            return response([
+                'error' => 'The provided credentials are not correct',
+
+            ], 422);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+}
