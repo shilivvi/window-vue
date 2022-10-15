5ba35d5577cf434e3fcbdf94ac3728b03057aa14
shilivvi <shilivvi98@gmail.com>
2022-10-15 22:06:16 +0300

    Added Vue3 and added some configuration for laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,17 +1,13 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix
+  .alias({ '@': 'resources/js' })
+  .js('resources/js/app.js', 'public/js')
+  .vue(3)
+  .sass('resources/scss/app.scss', 'public/css')
+  .options({
+    postCss: [
+      require('autoprefixer'),
+      require('postcss-sort-media-queries')
+    ]
+  })
