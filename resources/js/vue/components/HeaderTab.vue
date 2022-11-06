1ecda90ee9067c1354070282d862cc3989fc7d09
shilivvi <shilivvi98@gmail.com>
2022-11-06 23:12:53 +0300

    Added HeaderTab component

---
+++ resources/js/vue/components/HeaderTab.vue
@@ -0,0 +1,28 @@
+<template>
+  <li class="tabs-header__tab">
+    <span class="tabs-header__title">
+      <p>
+        <slot/>
+      </p>
+    </span>
+    <button
+      class="tabs-header__close btn-reset btn-del-window"
+      @click.prevent.stop="$emit('deleteWindow', props.id)"
+    >
+      <img src="img/close.svg" alt="Удалить окно">
+    </button>
+  </li>
+</template>
+
+<script setup>
+  import {defineEmits, defineProps} from "vue";
+
+  const props = defineProps({
+    id: {
+      type: Number,
+      required: true
+    }
+  })
+
+  const emit = defineEmits(['deleteWindow'])
+</script>
